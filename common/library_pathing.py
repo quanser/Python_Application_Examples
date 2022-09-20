650501f9bf71e79851d34965e85b6ace04aa9803
Murtaza Bohra <90704759+mBohra42@users.noreply.github.com>
2022-09-20 11:33:10 -0400

    Bug fix in pathing
    removed Quanser tag in the Python_application_libraries reference.

--- common/library_pathing.py
+++ common/library_pathing.py
@@ -1,7 +1,7 @@
 import sys
 
 def append_path(pathToCommon):  
-    pythonApplicationLibrariesPath = '../'+pathToCommon[0:len(pathToCommon)-7]+'Quanser_python_application_libraries/'
+    pythonApplicationLibrariesPath = '../'+pathToCommon[0:len(pathToCommon)-7]+'Python_application_libraries/'
     sys.path.append(pythonApplicationLibrariesPath+'application_modules/')
     sys.path.append(pythonApplicationLibrariesPath+'quanser_products/')
     sys.path.append(pythonApplicationLibrariesPath+'utilities/')
