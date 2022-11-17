95656f405c0b1c870940b49d537e3f1a025edca4
Murtaza Bohra <90704759+mBohra42@users.noreply.github.com>
2022-11-17 10:58:52 -0500

    Update library_pathing.py
    Updated the pathing library to reflect the correct Python_Application_Library repository context.

--- common/library_pathing.py
+++ common/library_pathing.py
@@ -1,8 +1,7 @@
 import sys
 
 def append_path(pathToCommon):  
-    pythonApplicationLibrariesPath = '../'+pathToCommon[0:len(pathToCommon)-7]+'Python_application_libraries/'
+    pythonApplicationLibrariesPath = '../'+pathToCommon[0:len(pathToCommon)-7]+'Python_Application_Libraries/'
     sys.path.append(pythonApplicationLibrariesPath+'application_modules/')
     sys.path.append(pythonApplicationLibrariesPath+'quanser_products/')
     sys.path.append(pythonApplicationLibrariesPath+'utilities/')
-    sys.path.append(pythonApplicationLibrariesPath+'quanser_interactive_labs/')
